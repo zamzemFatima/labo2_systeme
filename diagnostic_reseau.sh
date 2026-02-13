5730a86d0d06c5da0ce1956b8d259905330ce227
zamzam fatima <zamzem08@gmail.com>
2026-02-13 04:57:16 -0500

    ajout de lien github et partie 4

--- diagnostic_reseau.sh
+++ diagnostic_reseau.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+echo "information systeme"
+echo"------------------------"
+echo " Nom de l'hote : $(hostname)"
+echo " Date et heure : $(date)"
+echo "version de systeme : $(uname -a)
+echo ""
+
+echo "configuration reseaux"
+
+IP_LOCAL=$(hostname -I | awk '{print $1}')
+echo " Adresse IP locale :$IP_LOCAL"
+
+PASSSERELLE=$(ip route | grep default | awk '{print$3}')
+echo "
+
+
 
 
 
